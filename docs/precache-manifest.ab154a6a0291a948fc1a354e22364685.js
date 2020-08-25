de15391883a2eb9b0eaaa174e0572756542d80b7
Peacock-Co <pavaoleonardo@hotmail.com>
2020-08-25 10:42:04 +0200

    Docs added

---
+++ docs/precache-manifest.ab154a6a0291a948fc1a354e22364685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c43b487db52e55982a9f20b3662a03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f96a9421be7892044a5e",
+    "url": "/static/css/main.6185ab79.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96a9421be7892044a5e",
+    "url": "/static/js/main.5d6534f8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
